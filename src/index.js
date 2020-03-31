f0865de71f2de6cba6ac8db9b0b84e4bf435de5a
dinnosaur <dinnoroni@gmail.com>
2020-03-31 13:16:56 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submitForm = document.querySelector(`#create-task-form`);
+  const task = document.querySelector(`#tasks`)
+
+  submitForm.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    const input = document.querySelector(`#new-task-description`)
+    const taksLi = document.createElement(`li`)
+    taksLi.innerText = input.value
+    task.append(taksLi)
+  }) 
+
+  
+
+  
+  
 });
