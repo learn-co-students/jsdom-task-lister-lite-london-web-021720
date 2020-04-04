44550c6f80f4c0760a2810083bcf08db57dce417
Amaterasus <skyetay03@googlemail.com>
2020-04-04 11:47:31 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const taskForm = document.getElementById("create-task-form")
+const taskFormInput = document.getElementById("new-task-description")
+const list = document.getElementById("tasks")
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  taskForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const newTask = document.createElement("li")
+    newTask.innerText = taskFormInput.value
+    list.append(newTask)
+
+    taskForm.reset()
+
+    const deletebtn = document.createElement("button")
+    deletebtn.innerText = "X"
+    newTask.append(deletebtn)
+
+    deletebtn.addEventListener("click", e => newTask.remove())
+  })
 });
